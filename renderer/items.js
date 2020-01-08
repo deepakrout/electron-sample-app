eeca2b2ab348a7751ef3501598867eddce19a296
Deepak Rout <deepakrout@gmail.com>
2020-01-07 22:47:31 -0500

    Some additional changes

--- renderer/items.js
+++ renderer/items.js
@@ -1,8 +1,71 @@
+// Modules
+const fs = require('fs')
+
 // DOM nodes
 let items = document.getElementById('items')
 
+// Get readerJS contents
+let readerJS
+fs.readFile(`${__dirname}/reader.js`, (err, data) => {
+    readerJS = data.toString()
+})
+
+
+
 // Track items in storage
 exports.storage = JSON.parse(localStorage.getItem('readit-items')) || []
+
+// Listen for "Done" message from reader window
+window.addEventListener('message', e => {
+
+    // Check for correct message
+    if (e.data.action === 'delete-reader-item') {
+
+        // Delete item at given index
+        this.delete(e.data.itemIndex)
+
+        // Close the reader window
+        e.source.close()
+    }
+})
+
+exports.delete = itemIndex => {
+
+    // Remove item from DOM
+    items.removeChild(items.childNodes[itemIndex])
+
+    // Remove from storage
+    this.storage.splice(itemIndex, 1)
+
+    // Persist
+    this.save()
+
+    // Select previous item or new first item if first was deleted
+    if (this.storage.length) {
+
+        // Get new selected item index
+        let newSelectedItemIndex = (itemIndex === 0) ? 0 : itemIndex - 1
+
+        // Set item at new index as selected
+        document.getElementsByClassName('read-item')[newSelectedItemIndex].classList.add('selected')
+    }
+}
+
+// Get selected item index
+exports.getSelectedItem = () => {
+
+    // Get selected node
+    let currentItem = document.getElementsByClassName('read-item selected')[0]
+
+    // Get item index
+    let itemIndex = 0
+    let child = currentItem
+    while ((child = child.previousSibling) != null) itemIndex++
+
+        // Return selected item and index
+        return { node: currentItem, index: itemIndex }
+}
+
 
 // Persist storage
 exports.save = () => {
@@ -48,7 +111,19 @@ exports.open = () => {
     // Get item's url
     let contentURL = selectedItem.dataset.url
 
-    console.log('Opening item: ', contentURL)
+    // Open item in proxy BrowserWindow
+    let readerWin = window.open(contentURL, '', `
+    maxWidth=2000,
+    maxHeight=2000,
+    width=1200,
+    height=800,
+    backgroundColor=#DEDEDE,
+    nodeIntegration=0,
+    contextIsolation=1
+  `)
+
+    // Inject JavaScript
+    readerWin.eval(readerJS)
 }
 
 // Add new item
